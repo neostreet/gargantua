f6ae6409946398ac99e85d7e23c4830bceeb71d4
neostreet <lloyd.aidan@gmail.com>
2022-10-02 00:18:25 -0400

    Add files via upload

---
+++ common/gargantua.h
@@ -0,0 +1,33 @@
+#define IDM_NEW                100
+#define IDM_OPEN               101
+#define IDM_OPEN_BINARY_GAME   102
+#define IDM_SAVE               103
+#define IDM_SAVEAS             104
+#define IDM_PRINT              105
+#define IDM_PRINTSETUP         106
+#define IDM_EXIT               107
+
+#define IDI_GARGANTUA          108
+#define IDI_SMALL              109
+
+#define IDB_TOOLBAR            110
+
+#define IDM_SET_LAST_MOVE      200
+
+#define IDM_TOGGLE_ORIENTATION 201
+#define IDM_TOGGLE_BOARD_SIZE  202
+
+#define IDM_PREV_MOVE          204
+#define IDM_NEXT_MOVE          205
+#define IDM_START_OF_GAME      206
+#define IDM_END_OF_GAME        207
+#define IDM_FPRINT_GAME_BIN    208
+#define IDM_FPRINT_GAME        209
+#define IDM_FPRINT_BD          210
+
+#define IDM_ABOUT              211
+
+#define IDC_STATIC -1
+
+#define DLG_VERFIRST        400
+#define IDC_COMPANY        DLG_VERFIRST
