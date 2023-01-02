1fae1ad0a86e84528df3c5b476d7a0d91ef39837
neostreet <lloyd.aidan@gmail.com>
2023-01-01 19:49:45 -0500

    put gargantuas in the corners, and switch places with the kings and queens

--- common/gargmsc.c
+++ common/gargmsc.c
@@ -6,7 +6,7 @@
 #include "bitfuns.h"
 
 static unsigned char initial_board[] = {
-  (unsigned char)0x27, (unsigned char)0x34, (unsigned char)0x65, (unsigned char)0x43, (unsigned char)0x72,
+  (unsigned char)0x72, (unsigned char)0x34, (unsigned char)0x56, (unsigned char)0x43, (unsigned char)0x27,
   (unsigned char)0x11, (unsigned char)0x11, (unsigned char)0x11, (unsigned char)0x11, (unsigned char)0x11,
   (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
   (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
@@ -15,7 +15,7 @@ static unsigned char initial_board[] = {
   (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
   (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
   (unsigned char)0xff, (unsigned char)0xff, (unsigned char)0xff, (unsigned char)0xff, (unsigned char)0xff,
-  (unsigned char)0xe9, (unsigned char)0xdc, (unsigned char)0xab, (unsigned char)0xcd, (unsigned char)0x9e
+  (unsigned char)0x9e, (unsigned char)0xdc, (unsigned char)0xba, (unsigned char)0xcd, (unsigned char)0xe9
 };
 
 extern char piece_ids[]; /* "RNBQK" */
