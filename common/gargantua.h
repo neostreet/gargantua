bf9dec0ce4d9f3d47130170ab7eb11277678a510
neostreet <lloyd.aidan@gmail.com>
2023-02-27 23:43:59 -0500

    add IDM_PRINT_mOVES

--- common/gargantua.h
+++ common/gargantua.h
@@ -15,16 +15,17 @@
 #define IDM_SET_LAST_MOVE      200
 #define IDM_PRINT_BOARD        201
 #define IDM_PRINT_GAME         202
+#define IDM_PRINT_MOVES        203
 
-#define IDM_TOGGLE_ORIENTATION 203
-#define IDM_TOGGLE_BOARD_SIZE  204
+#define IDM_TOGGLE_ORIENTATION 204
+#define IDM_TOGGLE_BOARD_SIZE  205
 
-#define IDM_PREV_MOVE          205
-#define IDM_NEXT_MOVE          206
-#define IDM_START_OF_GAME      207
-#define IDM_END_OF_GAME        208
+#define IDM_PREV_MOVE          206
+#define IDM_NEXT_MOVE          207
+#define IDM_START_OF_GAME      208
+#define IDM_END_OF_GAME        209
 
-#define IDM_ABOUT              209
+#define IDM_ABOUT              210
 
 #define IDC_STATIC -1
 
