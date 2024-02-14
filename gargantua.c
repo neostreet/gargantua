fb40d346208df23f044f45ea7fb2248fd1f348d5
neostreet <lloyd.aidan@gmail.com>
2024-02-14 00:18:24 -0500

    restore the board to black in the lower right corner

--- gargantua.c
+++ gargantua.c
@@ -380,9 +380,9 @@ int get_piece_offset(int piece,int rank,int file)
        as pieces
     */
     if (!((rank + file) % 2))
-      retval = 24;
-    else
       retval = 25;
+    else
+      retval = 24;
   }
   else {
     if (piece < 0) {
@@ -399,7 +399,7 @@ int get_piece_offset(int piece,int rank,int file)
       if (bBlack)
         offset += 2;
 
-      if ((rank + file) % 2)
+      if (!((rank + file) % 2))
         offset++;
 
       retval = offset;
