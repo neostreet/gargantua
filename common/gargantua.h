233244ba02361bc34561c19590f5d5b6b2fd0141
neostreet <lloyd.aidan@gmail.com>
2024-06-21 21:56:56 -0400

    add IDM_GO_TO_MOVE_NUMBER and IDC_MOVE_NUMBER

--- common/gargantua.h
+++ common/gargantua.h
@@ -16,21 +16,23 @@
 #define IDM_PRINT_BOARD        201
 #define IDM_PRINT_GAME         202
 #define IDM_PRINT_MOVES        203
+#define IDM_GO_TO_MOVE_NUMBER  204
 
-#define IDM_TOGGLE_ORIENTATION 204
-#define IDM_TOGGLE_AUTO_SAVE   205
+#define IDM_TOGGLE_ORIENTATION 205
+#define IDM_TOGGLE_AUTO_SAVE   206
 
-#define IDM_PREV_MOVE          206
-#define IDM_NEXT_MOVE          207
-#define IDM_START_OF_GAME      208
-#define IDM_END_OF_GAME        209
+#define IDM_PREV_MOVE          207
+#define IDM_NEXT_MOVE          208
+#define IDM_START_OF_GAME      209
+#define IDM_END_OF_GAME        210
 
-#define IDM_ABOUT              210
+#define IDM_ABOUT              211
 
-#define IDM_PREV_GAME          211
-#define IDM_NEXT_GAME          212
+#define IDM_PREV_GAME          212
+#define IDM_NEXT_GAME          213
 
 #define IDC_STATIC -1
 
 #define DLG_VERFIRST        400
 #define IDC_COMPANY        DLG_VERFIRST
+#define IDC_MOVE_NUMBER    DLG_VERFIRST+1
